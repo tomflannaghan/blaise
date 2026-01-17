ec9c028de33d9d1a4a3997ab743ea96bc0de3664
Tom Flannaghan <tom@flannaghan.com>
2026-01-17 13:02:10 +0000

    Normalise word dist from corpus

--- src/blaise/data/worddist.py
+++ src/blaise/data/worddist.py
@@ -28,10 +28,12 @@ def _generate_from_corpus(name, overwrite=False):
         print("No corp")
     print(f"Calculating word distribution from corpus {name}")
     dist = dict(Counter(normalize_string(word) for word in corpus.split()))
+    # Some substrings are entirely non-alpha characters so remove these.
     if "" in dist:
-        dist.pop(
-            ""
-        )  # Some substrings are entirely non-alpha characters so remove these.
+        dist.pop("")
+    # Normalise word distribution
+    total = sum(dist.values())
+    dist = {w: n / total for w, n in dist.items()}
     save_word_dist(dist, name)
     print("Saved word distribution")
     return dist
